ecc22d56a68aa8a883060cb7d7ddfbc36c8a3a22
Jean <jeanmatheus0612@gmail.com>
2025-01-09 21:24:15 -0300

    Criando a variavel nomeReceita para receber o valor digitado no Scanner

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner leitura = new Scanner (System.in);
+        System.out.println("Digite o nome de uma receita: ");
+        var nomeReceita = leitura.nextLine();
 
-        
     }
 }
