4cac713a52454a69ac6c0c383feb56f17aea803e
Jean <jeanmatheus0612@gmail.com>
2025-01-09 21:26:11 -0300

    Utilizando o HTTP client e o request

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.Scanner;
 
 public class Principal {
@@ -5,6 +8,13 @@ public class Principal {
         Scanner leitura = new Scanner (System.in);
         System.out.println("Digite o nome de uma receita: ");
         var nomeReceita = leitura.nextLine();
+
+
+        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://foo.com/"))
+            .build();
 
     }
 }
